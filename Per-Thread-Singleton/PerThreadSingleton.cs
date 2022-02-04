ed7c023d376234f0d4d15e94d83dd09b02528314
Farook M Fazrin <nirzaf1@hotmail.com>
2022-02-04 09:18:06 +0530

    Add project files.

---
+++ Per-Thread-Singleton/PerThreadSingleton.cs
@@ -0,0 +1,15 @@
+﻿public sealed class PerThreadSingleton
+{
+    private static ThreadLocal<PerThreadSingleton> threadInstance
+        = new ThreadLocal<PerThreadSingleton>(
+            () => new PerThreadSingleton());
+
+    public int Id;
+
+    private PerThreadSingleton()
+    {
+        Id = Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public static PerThreadSingleton Instance => threadInstance.Value;
+}
---
+++ Per-Thread-Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+
+Task t1 = Task.Factory.StartNew(() =>
+{
+    WriteLine($"t1: " + PerThreadSingleton.Instance.Id);
+});
+Task t2 = Task.Factory.StartNew(() =>
+{
+    WriteLine($"t2: " + PerThreadSingleton.Instance.Id);
+    WriteLine($"t2 again: " + PerThreadSingleton.Instance.Id);
+});
+Task.WaitAll(t1, t2);
